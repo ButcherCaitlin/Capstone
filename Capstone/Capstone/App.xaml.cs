c5ea51143c14fa3051a13b7cbcf71d1d3e3b54a8
Tanner Wright <tannergtag@gmail.com>
2020-10-10 17:15:06 -0600

    Added the navigation service class and view names classes this will enable our app to navigate to other pages with dependency injection. I also removed the main page from the applacation. I also renamed the property epxlorer file.

--- Capstone/Capstone/App.xaml.cs
+++ Capstone/Capstone/App.xaml.cs
@@ -1,19 +1,24 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Capstone.Services;
 
 using Capstone.Views;
+using Capstone.Utility;
 
 namespace Capstone
 {
     public partial class App : Application
     {
+        //public static MockPropertDataStore dataService { get; } = new MockPropertDataStore();
+        public NavigationService navService => DependencyService.Get<NavigationService>() ?? new NavigationService();
+
         public App()
         {
             InitializeComponent();
 
-            //MainPage = new MainPage(); REMOVE THIS AFTER TESTING
-            MainPage = new NavigationPage(new PropertyExplorer());
+            navService.Configure(ViewNames.PropertyExplorerView, typeof(PropertyExplorerView));
+            MainPage = new NavigationPage(new PropertyExplorerView()); //this will need to be changed after we integrate the other pages.
         }
 
         protected override void OnStart()
---
+++ Capstone/Capstone/Services/INavigationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Capstone.Services
+{
+    public interface INavigationService
+    {
+        Page MainPage { get; }
+        void Configure(string key, Type pageType);
+        void GoBack();
+        void NavigateTo(string pageKey, object paramter = null);
+    }
+}
