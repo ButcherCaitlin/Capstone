6b57311adc0d7b129936362ca1f5f609f859985e
Tanner Wright <tannergtag@gmail.com>
2020-10-10 15:30:21 -0600

    Added the view model locator class to be used for dependency injection later on.

---
+++ Capstone/Capstone/Utility/ViewModelLocator.cs
@@ -0,0 +1,13 @@
+﻿using CapstoneXamarin.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Utility
+{
+    public static class ViewModelLocator
+    {
+        public static PropertyExplorerViewModel PropertyExplorerViewModel { get; set; }
+        = new PropertyExplorerViewModel();
+    }
+}
